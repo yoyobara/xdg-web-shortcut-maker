54c4422eab31cac0b3a01801351bac8e0d994543
yoyobara <yotambarak6@gmail.com>
2024-12-10 23:07:03 +0200

    created a basic dark theme

--- src/renderer/src/components/App.tsx
+++ src/renderer/src/components/App.tsx
@@ -1,17 +1,5 @@
-import { FormControlLabel, Switch } from "@mui/material";
-import { SyntheticEvent, useState } from "react";
-
 export function App(): JSX.Element {
-	const [isChecked, setChecked] = useState(true);
-
-	const handleSwitchChange = (_: SyntheticEvent, checked: boolean) => {
-		setChecked(checked);
-	}
-
 	return (
-		<div>
-			<FormControlLabel checked={isChecked} onChange={handleSwitchChange} control={<Switch/>} label="hello!"/>
-			<h1>the switch is {isChecked ? "checked" : "not checked"}</h1>
-		</div>
+		<div>hey</div>
 	)
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import { App } from './components'
 
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+
+		primary: {
+			main: "#37CFAB"
+		},
+
+		secondary: {
+			main: "#E05E2A"
+		},
+
+		success: {
+			main: "#B6EF3B"
+		},
+
+		info: {
+			main: "#298BBF"
+		},
+
+		warning: {
+			main: "#F1C138"
+		},
+
+		error: {
+			main: "#FB591F"
+		}
+	}
+});
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 )
